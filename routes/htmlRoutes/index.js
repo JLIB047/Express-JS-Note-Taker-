2e3a32f5c5c27ec27bbc57a21f722e94e4c256cf
Jason Libertelli <jason.libertelli@gmail.com>
2021-12-10 15:52:46 -0500

    ENOENT error

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,6 +3,14 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
-})
+});
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+//const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
-
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-//api routes
-app.use('/', htmlRoutes)
+//app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
 app.listen(PORT, () => {
